558a15cc1fe55500b2b93833d9834014f6884341
Masamunr <github.underpass782@passmail.com>
2026-08-28 20:45:37 +0100

    Add isolated map spike activity

---
+++ app/src/main/java/com/masamunr/trailcharter/map/MapSpikeActivity.kt
@@ -0,0 +1,20 @@
+package com.masamunr.trailcharter.map
+
+import android.os.Bundle
+import androidx.activity.ComponentActivity
+import androidx.activity.compose.setContent
+import androidx.activity.enableEdgeToEdge
+import com.masamunr.trailcharter.ui.theme.TrailCharterTheme
+
+/** Activity used only by the side-by-side debug renderer spike. */
+class MapSpikeActivity : ComponentActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        enableEdgeToEdge()
+        setContent {
+            TrailCharterTheme {
+                MapSpikeScreen()
+            }
+        }
+    }
+}
