baa0a3f73fb69a1b340575c3fcb5625367458064
Masamunr <github.underpass782@passmail.com>
2026-08-28 18:07:20 +0100

    Wire Adventure repository into app

--- app/src/main/java/com/masamunr/trailcharter/MainActivity.kt
+++ app/src/main/java/com/masamunr/trailcharter/MainActivity.kt
@@ -4,15 +4,21 @@ import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.activity.enableEdgeToEdge
+import com.masamunr.trailcharter.data.adventure.AdventureRepository
+import com.masamunr.trailcharter.data.adventure.TrailCharterDatabase
 import com.masamunr.trailcharter.ui.theme.TrailCharterTheme
 
 class MainActivity : ComponentActivity() {
+    private val adventureRepository by lazy {
+        AdventureRepository(TrailCharterDatabase.getInstance(this).adventureDao())
+    }
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         enableEdgeToEdge()
         setContent {
             TrailCharterTheme {
-                TrailCharterApp()
+                TrailCharterApp(repository = adventureRepository)
             }
         }
     }
